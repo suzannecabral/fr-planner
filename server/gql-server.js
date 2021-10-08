f5e55bc9f3ef0d2260e89644c8452552a458f75a
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-10-08 13:36:19 -0700

    initialize server, eslint settings

---
+++ server/gql-server.js
@@ -0,0 +1,8 @@
+const { ApolloServer } = require("apollo-server");
+
+require("dotenv").config();
+
+// graphql server
+// types: query / mutation / subscription
+// resolvers
+// function that resolves the quwery
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+// express server
+const app = express();
+
+// rest endpoint
+app.get("/api", (req, res) => {
+	res.json({
+		data: "The server is running, better go catch it",
+	});
+});
+
+// port
+app.listen(process.env.PORT, () => {
+	console.log(`server is ready at http://localhost:${process.env.PORT}`);
+});
