81722ee5eb8a8a5e00ae585e1d3cdcf3967f680e
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-10-29 16:24:20 -0700

    create react app, add dependencies

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+	return (
+		<div>
+			<Typography>Lair Planner</Typography>
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/api/fakeDragons.js
@@ -0,0 +1,15 @@
+// name
+// breed
+// gender
+// color1
+// gene1
+// color2
+// gene2
+// color3
+// gene3
+
+let fakeDragons = {
+	name: "asdf",
+};
+
+export default fakeDragons;
