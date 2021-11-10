1173f86840040e48bbd4b773eddd8f5dc4613953
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-11-09 22:14:17 -0800

    remove util, add papaparse

--- client/src/pages/Uploader.js
+++ client/src/pages/Uploader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import { CSVReader } from "react-papaparse";
 
 const Uploader = (props) => {
 	return (
--- client/src/util/parseCsv.js
+++
@@ -1,7 +0,0 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as csv from "fast-csv";
-
-const parseCsv = (file) => {
-	fs.createReadStream(path.resolve(__dirname, "asdf", "asdf.csv"));
-};
