fc2c7f9c5f7ff454e0ae68278e139d93407117e1
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-11-09 22:48:50 -0800

    add papaparse demo

--- client/src/pages/Uploader.js
+++ client/src/pages/Uploader.js
@@ -3,9 +3,33 @@ import { Box, Typography } from "@mui/material";
 import { CSVReader } from "react-papaparse";
 
 const Uploader = (props) => {
+	const handleDrop = (data) => {
+		console.log("--------------------");
+		console.log(data);
+		console.log("--------------------");
+	};
+
+	const handleError = (err, file, inputElem, reason) => {
+		console.log(err);
+	};
+
+	const handleRemoveFile = (data) => {
+		console.log("--------------------");
+		console.log(data);
+		console.log("--------------------");
+	};
+
 	return (
 		<Box>
-			<Typography>Uploader Component</Typography>
+			<Typography variant="h2">Drag & Drop Upload</Typography>
+			<CSVReader
+				onDrop={handleDrop}
+				onError={handleError}
+				addRemoveButton
+				onRemoveFile={handleRemoveFile}
+			>
+				<Typography>Drop CSV file here or click to upload.</Typography>
+			</CSVReader>
 		</Box>
 	);
 };
